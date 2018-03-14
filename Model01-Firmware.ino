b00d78a55f5d3d979b09d60002dc59836bff26a7
Sean Kelly <sean.kelly.2992@gmail.com>
2018-03-14 00:39:06 -0700

    Make layout more Atreus-like

--- Model01-Firmware.ino
+++ Model01-Firmware.ino
@@ -1,6 +1,18 @@
 // -*- mode: c++ -*-
 // Copyright 2016 Keyboardio, inc. <jesse@keyboard.io>
 // See "LICENSE" for license details
+// This is cbgbt's fork of the default Model01 Firmware.
+//
+// These keymaps are largely inspired by the original default layout of the
+// Atreus keyboard.
+// Changes:
+// - Change QWERTY layer to emulate Atreus more closely on RHS of right hand
+// - Add Atreus support to LHS of NUMPAD layer
+// - Fix RHS of Numpad layer to be more Atreus-like
+// - Add ATREMU layer to emulate the bottom row of keys on an Atreus in the bottom of Keyboardio (keyboardio homerow is lower than Atreus)
+// - Add DigitalRain plugin
+// - Remove mouse support
+// - Remove a few default LED plugins
 
 #ifndef BUILD_INFORMATION
 #define BUILD_INFORMATION "locally built"
@@ -13,50 +25,16 @@
  */
 
 
-// The Kaleidoscope core
 #include "Kaleidoscope.h"
-
-// Support for keys that move the mouse
-#include "Kaleidoscope-MouseKeys.h"
-
-// Support for macros
 #include "Kaleidoscope-Macros.h"
-
-// Support for controlling the keyboard's LEDs
 #include "Kaleidoscope-LEDControl.h"
-
-// Support for "Numpad" mode, which is mostly just the Numpad specific LED mode
-#include "Kaleidoscope-NumPad.h"
-
-// Support for an "LED off mode"
 #include "LED-Off.h"
-
-// Support for the "Boot greeting" effect, which pulses the 'LED' button for 10s
-// when the keyboard is connected to a computer (or that computer is powered on)
 #include "Kaleidoscope-LEDEffect-BootGreeting.h"
-
-// Support for LED modes that set all LEDs to a single color
 #include "Kaleidoscope-LEDEffect-SolidColor.h"
-
-// Support for an LED mode that makes all the LEDs 'breathe'
 #include "Kaleidoscope-LEDEffect-Breathe.h"
-
-// Support for an LED mode that makes a red pixel chase a blue pixel across the keyboard
 #include "Kaleidoscope-LEDEffect-Chase.h"
-
-// Support for LED modes that pulse the keyboard's LED in a rainbow pattern
 #include "Kaleidoscope-LEDEffect-Rainbow.h"
-
-// Support for an LED mode that lights up the keys as you press them
-#include "Kaleidoscope-LED-Stalker.h"
-
-// Support for an LED mode that prints the keys you press in letters 4px high
-#include "Kaleidoscope-LED-AlphaSquare.h"
-
-// Support for Keyboardio's internal keyboard testing mode
-#include "Kaleidoscope-Model01-TestMode.h"
-
-// Support for host power management (suspend & wakeup)
+#include "Kaleidoscope-LEDEffect-DigitalRain.h"
 #include "Kaleidoscope-HostPowerManagement.h"
 
 
@@ -79,29 +57,10 @@ enum { MACRO_VERSION_INFO,
 
 
 
-/** The Model 01's key layouts are defined as 'keymaps'. By default, there are three
-  * keymaps: The standard QWERTY keymap, the "Function layer" keymap and the "Numpad"
-  * keymap.
+/** The Model 01's key layouts are defined as 'keymaps'.
   *
   * Each keymap is defined as a list using the 'KEYMAP_STACKED' macro, built
   * of first the left hand's layout, followed by the right hand's layout.
-  *
-  * Keymaps typically consist mostly of `Key_` definitions. There are many, many keys
-  * defined as part of the USB HID Keyboard specification. You can find the names
-  * (if not yet the explanations) for all the standard `Key_` defintions offered by
-  * Kaleidoscope in these files:
-  *    https://github.com/keyboardio/Kaleidoscope/blob/master/src/key_defs_keyboard.h
-  *    https://github.com/keyboardio/Kaleidoscope/blob/master/src/key_defs_consumerctl.h
-  *    https://github.com/keyboardio/Kaleidoscope/blob/master/src/key_defs_sysctl.h
-  *    https://github.com/keyboardio/Kaleidoscope/blob/master/src/key_defs_keymaps.h
-  *
-  * Additional things that should be documented here include
-  *   using ___ to let keypresses fall through to the previously active layer
-  *   using XXX to mark a keyswitch as 'blocked' on this layer
-  *   using ShiftToLayer() and LockLayer() keys to change the active keymap.
-  *   the special nature of the PROG key
-  *   keeping NUM and FN consistent and accessible on all layers
-  *
   *
   * The "keymaps" data structure is a list of the keymaps compiled into the firmware.
   * The order of keymaps in the list is important, as the ShiftToLayer(#) and LockLayer(#)
@@ -109,63 +68,90 @@ enum { MACRO_VERSION_INFO,
   *
   *
 
-  * A key defined as 'ShiftToLayer(FUNCTION)' will switch to FUNCTION while held.
+  * A key defined as 'ShiftToLayer(KBIOFUNCTION)' will switch to KBIOFUNCTION while held.
   * Similarly, a key defined as 'LockLayer(NUMPAD)' will switch to NUMPAD when tapped.
   */
 
 /**
   * Layers are "0-indexed" -- That is the first one is layer 0. The second one is layer 1.
   * The third one is layer 2.
-  * This 'enum' lets us use names like QWERTY, FUNCTION, and NUMPAD in place of
+  * This 'enum' lets us use names like QWERTY, KBIOFUNCTION, and NUMPAD in place of
   * the numbers 0, 1 and 2.
   *
   */
 
-enum { QWERTY, NUMPAD, FUNCTION }; // layers
+enum { QWERTY, NUMPAD, ATREMU, KBIOFUNCTION }; // layers
 
 /* This comment temporarily turns off astyle's indent enforcement
  *   so we can make the keymaps actually resemble the physical key layout better
  */
 // *INDENT-OFF*
 
-const Key keymaps[][ROWS][COLS] PROGMEM = {
+/* Missing keys:
+ * QWERTY
+ * Key_LeftGui
+ * Key_PageUp, Key_PageDown
+ * Key_RightShift
+ *
+ * NUMPAD
+ * Key_KeypadSubtract, Key_KeypadDivide
+ */
 
+const Key keymaps[][ROWS][COLS] PROGMEM = {
   [QWERTY] = KEYMAP_STACKED
-  (___,          Key_1, Key_2, Key_3, Key_4, Key_5, Key_LEDEffectNext,
-   Key_Backtick, Key_Q, Key_W, Key_E, Key_R, Key_T, Key_Tab,
-   Key_PageUp,   Key_A, Key_S, Key_D, Key_F, Key_G,
-   Key_PageDown, Key_Z, Key_X, Key_C, Key_V, Key_B, Key_Escape,
-   Key_LeftControl, Key_Backspace, Key_LeftGui, Key_LeftShift,
-   ShiftToLayer(FUNCTION),
+  (___,           Key_1, Key_2, Key_3, Key_4, Key_5, Key_LEDEffectNext,
+   Key_Tab,       Key_Q, Key_W, Key_E, Key_R, Key_T, Key_Tab,
+   Key_Tab,       Key_A, Key_S, Key_D, Key_F, Key_G,
+   Key_Escape,    Key_Z, Key_X, Key_C, Key_V, Key_B, Key_Escape,
+   Key_LeftShift, Key_Backspace, Key_LeftControl, Key_Quote,
+   ShiftToLayer(ATREMU),
 
    M(MACRO_ANY),  Key_6, Key_7, Key_8,     Key_9,         Key_0,         LockLayer(NUMPAD),
    Key_Enter,     Key_Y, Key_U, Key_I,     Key_O,         Key_P,         Key_Equals,
                   Key_H, Key_J, Key_K,     Key_L,         Key_Semicolon, Key_Quote,
-   Key_RightAlt,  Key_N, Key_M, Key_Comma, Key_Period,    Key_Slash,     Key_Minus,
-   Key_RightShift, Key_LeftAlt, Key_Spacebar, Key_RightControl,
-   ShiftToLayer(FUNCTION)),
+   Key_RightAlt,  Key_N, Key_M, Key_Comma, Key_Period,    Key_Slash,     Key_Enter,
+   Key_Minus, Key_LeftAlt, Key_Spacebar, ShiftToLayer(NUMPAD),
+   ShiftToLayer(ATREMU)),
 
 
   [NUMPAD] =  KEYMAP_STACKED
   (___, ___, ___, ___, ___, ___, ___,
-   ___, ___, ___, ___, ___, ___, ___,
-   ___, ___, ___, ___, ___, ___,
-   ___, ___, ___, ___, ___, ___, ___,
-   ___, ___, ___, ___,
-   ___,
+   ___, LSHIFT(Key_1), LSHIFT(Key_2), LSHIFT(Key_LeftCurlyBracket), LSHIFT(Key_RightCurlyBracket), LSHIFT(Key_Backslash), ___,
+   ___, LSHIFT(Key_3), LSHIFT(Key_4), LSHIFT(Key_9),                LSHIFT(Key_0),                 Key_Backtick,
+   ___, LSHIFT(Key_5), LSHIFT(Key_6), Key_LeftCurlyBracket,         Key_RightCurlyBracket,         LSHIFT(Key_Backtick),  ___,
+                  LockLayer(KBIOFUNCTION), LSHIFT(Key_Insert), Key_PrintScreen, ___,
+                                           ___,
 
-   M(MACRO_VERSION_INFO),  ___, Key_Keypad7, Key_Keypad8,   Key_Keypad9,        Key_KeypadSubtract, ___,
-   ___,                    ___, Key_Keypad4, Key_Keypad5,   Key_Keypad6,        Key_KeypadAdd,      ___,
-                           ___, Key_Keypad1, Key_Keypad2,   Key_Keypad3,        Key_Equals,         Key_Quote,
-   ___,                    ___, Key_Keypad0, Key_KeypadDot, Key_KeypadMultiply, Key_KeypadDivide,   Key_Enter,
+   M(MACRO_VERSION_INFO),  Key_PageUp,    Key_Keypad7, Key_Keypad8,   Key_Keypad9,  Key_KeypadMultiply, ___,
+   ___,                    Key_PageDown,  Key_Keypad4, Key_Keypad5,   Key_Keypad6,  Key_KeypadAdd,      ___,
+                           LSHIFT(Key_7), Key_Keypad1, Key_Keypad2,   Key_Keypad3,  Key_Backslash,      Key_Quote,
+   ___,                    ___,           Key_Keypad0, Key_KeypadDot, Key_Keypad0,  Key_Equals,         Key_Enter,
    ___, ___, ___, ___,
-   ___),
+        ___),
 
-  [FUNCTION] =  KEYMAP_STACKED
+  // The atreus has an additional row of keys under the home row which add
+  // A few keys committed to my muscle memory. We make a special function row
+  // which allows for atreus-like pressing of these keys.
+  [ATREMU] = KEYMAP_STACKED
+  (___, ___,        ___,     ___, ___, ___, ___,
+   ___, ___,        ___,     ___, ___, ___, ___,
+   ___, ___,        ___,     ___, ___, ___,
+   ___, Key_Escape, Key_Tab, ___, ___, ___, ___,
+                    ___, ___, ___, ___,
+                         ___,
+
+   ___, ___, ___, ___,       ___,       ___,       ___,
+   ___, ___, ___, ___,       ___,       ___,       ___,
+        ___, ___, ___,       ___,       ___,       ___,
+   ___, ___, ___, Key_Minus, Key_Quote, Key_Enter, ___,
+   ___, ___, ___, ___,
+             ___),
+
+  [KBIOFUNCTION] =  KEYMAP_STACKED
   (___,      Key_F1,           Key_F2,      Key_F3,     Key_F4,        Key_F5,           XXX,
-   Key_Tab,  ___,              Key_mouseUp, ___,        Key_mouseBtnR, Key_mouseWarpEnd, Key_mouseWarpNE,
-   Key_Home, Key_mouseL,       Key_mouseDn, Key_mouseR, Key_mouseBtnL, Key_mouseWarpNW,
-   Key_End,  Key_PrintScreen,  Key_Insert,  ___,        Key_mouseBtnM, Key_mouseWarpSW,  Key_mouseWarpSE,
+   Key_Tab,  ___,              ___, ___,        ___, ___, ___,
+   Key_Home, ___,       ___, ___, ___, ___,
+   Key_End,  Key_PrintScreen,  Key_Insert,  ___,        ___, ___,  ___,
    ___, Key_Delete, ___, ___,
    ___,
 
@@ -279,24 +265,15 @@ void hostPowerManagementEventHandler(kaleidoscope::HostPowerManagement::Event ev
   toggleLedsOnSuspendResume(event);
 }
 
-/** The 'setup' function is one of the two standard Arduino sketch functions.
-  * It's called when your keyboard first powers up. This is where you set up
-  * Kaleidoscope and any plugins.
-  */
-
 void setup() {
-  // First, call Kaleidoscope's internal setup function
   Kaleidoscope.setup();
 
-  // Next, tell Kaleidoscope which plugins you want to use.
+  // Tell Kaleidoscope which plugins you want to use.
   // The order can be important. For example, LED effects are
   // added in the order they're listed here.
   Kaleidoscope.use(
-    // The boot greeting effect pulses the LED button for 10 seconds after the keyboard is first connected
+    // Pulse LED button for 10 seconds on startup.
     &BootGreetingEffect,
-
-    // The hardware test mode, which can be invoked by tapping Prog, LED and the left Fn button at the same time.
-    &TestMode,
 
     // LEDControl provides support for other LED modes
     &LEDControl,
@@ -307,6 +284,9 @@ void setup() {
     // The rainbow effect changes the color of all of the keyboard's keys at the same time
     // running through all the colors of the rainbow.
     &LEDRainbowEffect,
+
+    // Makes green digital rain
+    &LEDDigitalRainEffect,
 
     // The rainbow wave effect lights up your keyboard with all the colors of a rainbow
     // and slowly moves the rainbow across your keyboard
@@ -322,44 +302,20 @@ void setup() {
     // The breathe effect slowly pulses all of the LEDs on your keyboard
     &LEDBreatheEffect,
 
-    // The AlphaSquare effect prints each character you type, using your
-    // keyboard's LEDs as a display
-    &AlphaSquareEffect,
-
-    // The stalker effect lights up the keys you've pressed recently
-    &StalkerEffect,
-
-    // The numpad plugin is responsible for lighting up the 'numpad' mode
-    // with a custom LED effect
-    &NumPad,
-
     // The macros plugin adds support for macros
     &Macros,
-
-    // The MouseKeys plugin lets you add keys to your keymap which move the mouse.
-    &MouseKeys,
 
     // The HostPowerManagement plugin enables waking up the host from suspend,
     // and allows us to turn LEDs off when it goes to sleep.
     &HostPowerManagement
   );
 
-  // While we hope to improve this in the future, the NumPad plugin
-  // needs to be explicitly told which keymap layer is your numpad layer
-  NumPad.numPadLayer = NUMPAD;
-
-  // We configure the AlphaSquare effect to use RED letters
-  AlphaSquare.color = { 255, 0, 0 };
+  LEDDigitalRainEffect.DROP_TICKS = 22;
 
   // We set the brightness of the rainbow effects to 150 (on a scale of 0-255)
   // This draws more than 500mA, but looks much nicer than a dimmer effect
   LEDRainbowEffect.brightness(150);
   LEDRainbowWaveEffect.brightness(150);
-
-  // The LED Stalker mode has a few effects. The one we like is
-  // called 'BlazingTrail'. For details on other options,
-  // see https://github.com/keyboardio/Kaleidoscope-LED-Stalker
-  StalkerEffect.variant = STALKER(BlazingTrail);
 
   // We want the keyboard to be able to wake the host up from suspend.
   HostPowerManagement.enableWakeup();
@@ -369,13 +325,6 @@ void setup() {
   // with USB devices
   LEDOff.activate();
 }
-
-/** loop is the second of the standard Arduino sketch functions.
-  * As you might expect, it runs in a loop, never exiting.
-  *
-  * For Kaleidoscope-based keyboard firmware, you usually just want to
-  * call Kaleidoscope.loop(); and not do anything custom here.
-  */
 
 void loop() {
   Kaleidoscope.loop();
